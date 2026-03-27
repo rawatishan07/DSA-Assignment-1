94f96a6a82b1f69a73a42e8ba066da5c74c0ba45
Ishan Rawat <rawatishan510@gmail.com>
2026-03-27 12:32:43 +0530

    Update Q1.py

--- DSA Assignment/Q1.py
+++ DSA Assignment/Q1.py
@@ -1,13 +1,9 @@
-# Factorial using naive approach
 def factorial_naive(n: int) -> int:
     if n <= 1:
         return 1
     else:
         return n * factorial_naive(n-1)
-# The Time Complexity is O(n) because we must perform n multiplications
-# The Space Complexity is O(n) due to the recursion depth on the call stack
 
-# Factorial using memoized approach
 def factorial_memoized(n) -> int:
     global memo_factorial
     if memo_factorial[n] != -1: 
@@ -17,19 +13,13 @@ def factorial_memoized(n) -> int:
     else:
         memo_factorial[n] = n * factorial_memoized(n-1)
         return memo_factorial[n]
-# The Time Complexity is O(n) because we must perform n multiplications
-# The Space Complexity is O(n) due to the recursion depth on the call stack
 
-# Fibonacci using naive approach
 def fibonacci_naive(n: int) -> int:
     if n <= 1:
         return n
     else:
         return fibonacci_naive(n-1) + fibonacci_naive(n-2)
-# The Time Complexity is O(2^n) because of 2 recursion calls for every term
-# The Space Complexity is O(n) due to the recursion depth on the call stack
 
-# Fibonacci using memoized approach
 def fibonacci_memoized(n) -> int:
     global memo_fibonacci
     if memo_fibonacci[n] != -1: 
@@ -40,10 +30,7 @@ def fibonacci_memoized(n) -> int:
     else:
         memo_fibonacci[n] = fibonacci_memoized(n-1) + fibonacci_memoized(n-2)
         return memo_fibonacci[n]
-# The Time Complexity is O(n) because each term is only calculated once and memoized for future use
-# The Space Complexity is O(n) due to the recursion depth on the call stack
 
-# Empty lists to store the results
 memo_factorial = []
 memo_fibonacci = []
 
